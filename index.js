7746a708b096e2f47037205de0a5ae76ce111322
Jonny Burger <jonathanburger11@gmail.com>
2015-05-02 22:33:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
+  response.end("Served from " + require("os").hostname());
 });
 
 server.listen(5000);
